6487328c860d9f55dcc0901873c9de0673c75bc6
Mufid Afif <mufid@idmail.or.id>
2013-04-05 11:37:56 +0700

    Make ABS + nyoba buat service

--- src/com/mando/SettingsWelcome.java
+++ src/com/mando/SettingsWelcome.java
@@ -1,22 +1,20 @@
 package com.mando;
 
+import com.actionbarsherlock.app.SherlockActivity;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Button;
 
-public class SettingsWelcome extends Activity {
+public class SettingsWelcome extends SherlockActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_settings_welcome);
-	}
+		
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_settings_welcome, menu);
-		return true;
 	}
 
 }
---
+++ src/com/mando/service/MainService.java
@@ -0,0 +1,19 @@
+package com.mando.service;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class MainService extends IntentService {
+
+	public MainService(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void onHandleIntent(Intent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
