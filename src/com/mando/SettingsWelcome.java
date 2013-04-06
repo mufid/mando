c54a37442b5f36924c9cc736047af3b427dace52
Mufid Afif <mufid@idmail.or.id>
2013-04-06 18:16:47 +0700

    Feature complete: Settings save to persistent storage.

--- src/com/mando/SettingsWelcome.java
+++ src/com/mando/SettingsWelcome.java
@@ -1,11 +1,15 @@
 package com.mando;
 
+import java.util.Date;
+
 import com.actionbarsherlock.app.SherlockActivity;
+import com.mando.helper.SettingsHelper;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class SettingsWelcome extends SherlockActivity {
 
@@ -14,7 +18,24 @@ public class SettingsWelcome extends SherlockActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_settings_welcome);
 		
-
+		SettingsHelper.init(this);
+		String terakhir = SettingsHelper.read("terakhir");
+		String teks;
+		
+		if (terakhir == null) {
+			teks = getString(R.string.setting_terakhir_tidakpernah);
+		} else {
+			teks = String.format(getString(R.string.setting_terakhir), terakhir.toString());
+		}
+		
+		TextView welcomeLabel = (TextView) findViewById(R.id.welcome_welcometext);
+		welcomeLabel.setText(teks);
+		
+		SettingsHelper.store("terakhir", new Date().toString());
 	}
-
+	
+	@Override
+	protected void onDestroy() {
+		
+	}
 }
---
+++ src/com/mando/helper/SettingsHelper.java
@@ -0,0 +1,40 @@
+package com.mando.helper;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class SettingsHelper {
+	private static Context context;
+	private static String PREFERENCES_NAME = "MandoSettings";
+	
+	public static void init(Context c) {
+		context = c;
+	}
+	
+	/**
+	 * Menyimpan string ke Persistent Storage. Jangan lupa untuk melakukan
+	 * init terlebih dahulu
+	 * @param key
+	 * @param value
+	 */
+	public static void store(String key, String value) {
+		//Modification of the Preferences
+		SharedPreferences prefs = context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE);
+		SharedPreferences.Editor     Prefeditor = prefs.edit();
+		Prefeditor.putString(key, value);
+		Prefeditor.commit();
+	}
+	
+	/**
+	 * Membaca dari Persisten Storage. Mengembalikan <code>null</code>
+	 * apabila preferences belum pernah dibuat. Jangan lupa untuk melakukan
+	 * init terlebih dahulu
+	 * @param key
+	 * @return
+	 */
+	public static String read(String key) {
+		//Reading values from the Preferences
+		SharedPreferences prefs = context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE);
+		return prefs.getString(key, null);
+	}
+}
