3e3f57bead8d05736da7360c89bd27cc06f7cd61
Rizqi Synergy <rsfsr.cccp@gmail.com>
2013-04-08 16:14:53 +0700

    - Edit-edit padding. - Persiapan SMS & lokasi.

--- src/com/mando/service/MandoController.java
+++ src/com/mando/service/MandoController.java
@@ -4,31 +4,40 @@ import com.mando.helper.SMS;
 
 /**
  * Kerjain yang parser SMS
+ * 
  * @author Mufid
- *
+ * 
  */
 public class MandoController {
-	
+
 	/**
 	 * Parsing SMS yang masuk sekaligus menjalankan fungsi SMS yang masuk
+	 * 
 	 * @param s
 	 */
 	public static void processSMS(String s) {
 		// Ini yang ada di UCRS pertama
 	}
-	
+
 	public static String getContacts(String s) {
 		// TODO
 		return null;
 	}
+
+	public static String getLocation() {
+		return "";
+	}
+
 	public static String forwardSMS(SMS message) {
 		// TODO
 		return null;
 	}
+
 	public static String receiveSMS(int count) {
 		// TODO
 		return null;
 	}
+
 	public static String recordSound() {
 		return null;
 	}
